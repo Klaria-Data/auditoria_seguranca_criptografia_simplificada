5da3e1a7a0fc8a1ba38659d82b1f5fbf8f9b077c
Klaria-Data <klariaandrade@gmail.com>
2026-02-05 20:02:50 -0300

    Atualizando comentários da função

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import hashlib
 
-
 def GEN(seed: list[int]) -> list[int]:
     """
-    Função responsável por criar uma chave binária baseada em uma semente (lista de bits).
-    Regras:
-    1. Baseada na seed (lista de 0s e 1s).
-    2. Comprimento da chave: 4 * tamanho da semente.
+    Function responsible for generate the binary key.
+
+    :param seed: list of integer that will be used as key.
+    :return: a list of integer that will be used as key.
     """
     seed_str = "".join(map(str, seed))
     hash_obj = hashlib.sha256(seed_str.encode()).digest()
-
     key_len = 4 * len(seed)
 
     bits = []
@@ -18,28 +16,41 @@ def GEN(seed: list[int]) -> list[int]:
         for i in range(7, -1, -1):
             bits.append((byte >> i) & 1)
 
-    return bits[:key_len] if len(bits) >= key_len else (bits * (key_len // len(bits) + 1))[:key_len]
+    if len(bits) >= key_len:
+        result = bits[:key_len]
+    else:
+        result = (bits * (key_len // len(bits) + 1))[:key_len]
+
+    return result
 
 
 def ENC(K: list[int], M: list[int]) -> list[int]:
     """
-    Recebe a chave K e mensagem M e retorna a cifra C.
+    Function responsible for encrypting the message.
+
+    :param K: list of integer representing the key.
+    :param M: list of integer representing the message.
+    :return: a list of integer representing the cipher.
     """
     if len(K) != len(M):
         raise ValueError("K e M devem ter o mesmo tamanho (4 * len(seed)).")
 
-    return [k ^ m for k, m in zip(K, M)]
+    response = [k ^ m for k, m in zip(K, M)]
+    return response
 
 
 def DEC(K: list[int], C: list[int]) -> list[int]:
     """
-    Recebe a chave K e a cifra C e retorna a mensagem original m.
+    Function responsible for decrypting the message.
+
+    :param K: list of integer representing the key.
+    :param C: list of integer representing the cipher.
+    :return: a list of integer representing the original message.
     """
     return [k ^ c for k, c in zip(K, C)]
 
 
 def teste_difusao(seed_binaria, mensagem_original):
-    """Avalia o efeito da alteração de um único bit da mensagem na cifra."""
     K = GEN(seed_binaria)
     C1 = ENC(K, mensagem_original)
 
@@ -52,7 +63,6 @@ def teste_difusao(seed_binaria, mensagem_original):
 
 
 def teste_confusao(seed_original, mensagem):
-    """Avalia o impacto da alteração de um único bit na seed da chave na cifra C."""
     K1 = GEN(seed_original)
     C1 = ENC(K1, mensagem)
 
